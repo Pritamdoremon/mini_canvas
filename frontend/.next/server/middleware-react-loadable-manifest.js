40fb99e7040f4fac3695dda734ecaa4de48597ae
pritam <pritamroysarkar2003@gmail.com>
2026-09-15 01:20:44 +0530

    add

--- frontend/.next/server/middleware-react-loadable-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__REACT_LOADABLE_MANIFEST="{\"app\\\\page.jsx -> ../src/App.jsx\":{\"id\":\"app\\\\page.jsx -> ../src/App.jsx\",\"files\":[\"static/chunks/_app-pages-browser_src_App_jsx.js\"]}}"
